7d98bfbf75a80290d14799cdc729cf888c38093d
akmkamrulhasanreyad <akmkamrulhasanreyad@gmail.com>
2023-09-13 23:51:35 +0600

    close this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Watch from './Component/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [watches, setWatches] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('watches.json')
+  //     .then(res => res.json())
+  //     .then(data => setWatches(data));
+  // }, [])
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/reyad-hasan/watches-data/main/watches.json')
+      .then(res => res.json())
+      .then(data => setWatches(data));
+  }, [])
+
+  // const watches = [
+  //   {
+  //     "id": 1,
+  //     "name": "Apple Watch Series 7",
+  //     "price": 399.99
+  //   },
+  //   {
+  //     "id": 2,
+  //     "name": "Galaxy Watch 4",
+  //     "price": 249.99
+  //   },
+  //   {
+  //     "id": 3,
+  //     "name": "Fitbit Versa 3",
+  //     "price": 179.95
+  //   },
+  //   {
+  //     "id": 4,
+  //     "name": "Garmin Forerunner 945",
+  //     "price": 549.99
+  //   },
+  //   {
+  //     "id": 5,
+  //     "name": "Fossil Gen 5",
+  //     "price": 249.00
+  //   }
+  // ];
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
     </>
   )
 }
---
+++ src/Component/Sunglass/Sunglass.jsx
@@ -0,0 +1,9 @@
+const Sunglass = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Sunglass;
